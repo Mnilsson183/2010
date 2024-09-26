b17c9c88abd2a5d259b5493a2ded54a5e6182133
mnilsson183 <morgan.nilsson7111@icloud.com>
2024-09-26 14:48:24 -0600

    bad commit message here

--- 3lab/2/main.cpp
+++ 3lab/2/main.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+
 int main(void) {
+	double x, y;
+
+	std::cout << "Enter a value for X: ";
+	std::cin >> x;
+	std::cout << "Enter a value for Y: ";
+	std::cin >> y;
+
+	double* x_ptr = &x;
+	double* y_ptr = &y;
+	double sum = *x_ptr + *y_ptr;
+
+	std::cout << "X value: " << *x_ptr << "\nY value: " << *y_ptr << "\nSum: " << sum << '\n';
 	return 0;
 }
